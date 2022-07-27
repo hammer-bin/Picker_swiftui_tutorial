9f19ded8f3e55dcd376f5c5b9c38b03b22a403bb
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-07-28 06:57:19 +0900

    Initial Commit

---
+++ Picker_swiftui_tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Picker_swiftui_tutorial
+//
+//  Created by minkyuLee on 2022/07/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Picker_swiftui_tutorial/Picker_swiftui_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Picker_swiftui_tutorialApp.swift
+//  Picker_swiftui_tutorial
+//
+//  Created by minkyuLee on 2022/07/28.
+//
+
+import SwiftUI
+
+@main
+struct Picker_swiftui_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
